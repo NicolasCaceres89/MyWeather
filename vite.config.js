d9f98a39c8a6333c5dc3b161675423d335438043
NicolasCaceres89 <nicolas.caceres2839@gmail.com>
2025-12-07 02:27:34 -0600

    20251207_Modification for some .json, and also a .js, testing secure

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(),
+        laravel({
+            input: ["resources/js/app.js"],
+            refresh: true,
+        })
+    ],
     build: {
         outDir: 'public/build',
         emptyOutDir: true,
